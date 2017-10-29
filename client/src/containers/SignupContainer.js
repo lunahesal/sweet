096cc4b2a33d2871bf8bfc9ba0817901e7cbf996
lunahesal <1733766131@qq.com>
2017-10-29 21:14:27 +0000

    react and router

---
+++ client/src/containers/SignupContainer.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+class SignupContainer extends Component {
+  render() {
+    return (
+      <div>
+      SignupContainer
+      </div>
+    )
+  }
+}
+export default SignupContainer
